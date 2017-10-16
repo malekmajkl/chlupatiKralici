e33af0a3d932d3bfc1fae73b635ee541a7986a63
root <root@localhost>
2017-10-16 11:56:03 +0200

    chlupáči oprava

--- Kralici/src/main/java/com/kralici/jdbc/DataSourceTest.java
+++ Kralici/src/main/java/com/kralici/jdbc/DataSourceTest.java
@@ -11,26 +11,8 @@ import javax.sql.DataSource;
 public class DataSourceTest {
 public static void main(String[] args) {
 		
-		
-		DataSourcetest("oracle");
-
-	}
-
-	private static void DataSourcetest(String dbType) {
-	
-	
-
-
-	
-		DataSource ds = null;
-	
-		if("oracle".equals(dbType)){
-			ds = MyDataSourceFactory.getOracleDataSource();
-		}else{
-			System.out.println("invalid db type");
-			return;
-		}
-		
+		DataSource ds = MyDataSourceFactory.getOracleDataSource();
+				
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
